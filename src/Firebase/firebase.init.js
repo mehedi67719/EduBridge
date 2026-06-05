ad513c49e0823d2eca5a53031d8dba4926e7aa26
Mehedi Hassan <meh67719@gamil.com>
2026-06-06 00:38:37 +0600

    fix firebase error

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -1,13 +1,27 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDtfAjc7GABfh5SKwBt4wyFeV1KLqIAKis",
+
+  authDomain: "d-models-5490e.firebaseapp.com",
+
+  projectId: "d-models-5490e",
+
+  storageBucket: "d-models-5490e.firebasestorage.app",
+
+  messagingSenderId: "178455683857",
+
+  appId: "1:178455683857:web:c457461f650f0284e3a2b0",
 };
 
 const app = initializeApp(firebaseConfig);
